e7180832f1c44ec6954afd728ef0fe98d56fa735
Will Dean <wd60622@gmail.com>
2025-01-21 09:58:24 +0100

    use train test split

--- 04-pymc-marketing-mmm/__main__.py
+++ 04-pymc-marketing-mmm/__main__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from itertools import product
 from pathlib import Path
 
@@ -29,7 +30,26 @@ def read_data() -> pd.DataFrame:
     return pd.read_csv(data_url, parse_dates=["date_week"])
 
 
-def run_experiment(X, y, adstock_config, saturation_config, yearly_seasonality):
+@dataclass
+class Data:
+    X: pd.DataFrame
+    y: pd.Series
+
+    @classmethod
+    def from_df(cls, df, target_col):
+        return cls(
+            X=df.drop(target_col, axis=1),
+            y=df[target_col],
+        )
+
+
+@dataclass
+class Split:
+    train: Data
+    test: Data
+
+
+def run_experiment(split: Split, adstock_config, saturation_config, yearly_seasonality):
     adstock = adstock_from_dict(adstock_config)
     saturation = saturation_from_dict(saturation_config)
 
@@ -47,26 +67,57 @@ def run_experiment(X, y, adstock_config, saturation_config, yearly_seasonality):
     )
 
     with mlflow.start_run():
-        idata = mmm.fit(X, y, nuts_sampler="numpyro")
+        idata = mmm.fit(split.train.X, split.train.y, nuts_sampler="numpyro")
 
         for transform in [mmm.adstock, mmm.saturation, mmm.yearly_fourier]:
             curve = transform.sample_curve(idata.posterior)
             fig, _ = transform.plot_curve(curve)
             mlflow.log_figure(fig, f"{transform.prefix}_curve.png")
 
+        in_predictions = mmm.sample_posterior_predictive(
+            X_pred=split.train.X,
+        )
+        out_predictions = mmm.sample_posterior_predictive(
+            X_pred=split.test.X,
+            include_last_observations=True,
+        )
+
+        metrics_to_calculate = ["r_squared", "rmse"]
+        pymc_marketing.mlflow.log_mmm_evaluation_metrics(
+            y_true=split.train.y,
+            y_pred=in_predictions.y,
+            prefix="in-sample",
+            metrics_to_calculate=metrics_to_calculate,
+        )
+        pymc_marketing.mlflow.log_mmm_evaluation_metrics(
+            y_true=split.test.y,
+            y_pred=out_predictions.y,
+            prefix="out-sample",
+            metrics_to_calculate=metrics_to_calculate,
+        )
+
         pymc_marketing.mlflow.log_mmm(mmm=mmm)
 
 
-def run_experiments(X, y, combinations):
+def run_experiments(split: Split, combinations):
     for adstock_config, saturation_config, yearly_seasonality in combinations:
-        run_experiment(X, y, adstock_config, saturation_config, yearly_seasonality)
+        run_experiment(split, adstock_config, saturation_config, yearly_seasonality)
 
 
 def main():
     data = read_data()
 
-    X = data.drop("y", axis=1)
-    y = data["y"]
+    cutoff = "2021-01-01"
+
+    idx_train = data["date_week"] < cutoff
+
+    data_train = data.loc[idx_train]
+    data_test = data.loc[~idx_train]
+
+    split = Split(
+        train=Data.from_df(data_train, target_col="y"),
+        test=Data.from_df(data_test, target_col="y"),
+    )
 
     mlflow_set_tracking_uri()
     mlflow.set_experiment("04-pymc-marketing-mmm")
@@ -85,7 +136,7 @@ def main():
     )
     print(f"Running a combination of {len(combinations)} MMM models")
 
-    run_experiments(X, y, combinations)
+    run_experiments(split=split, combinations=combinations)
 
 
 if __name__ == "__main__":
