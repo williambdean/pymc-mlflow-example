9c2fdd19d7a5007842dc156ff87ea078528f75e7
Will Dean <wd60622@gmail.com>
2025-04-01 12:35:32 +0200

    put in main block

--- 03-pymc-autologging.py
+++ 03-pymc-autologging.py
@@ -21,31 +21,26 @@ import numpy as np
 
 import pymc_marketing.mlflow
 
+if __name__ == "__main__":
+    seed = sum(map(ord, "Logging PyMC model"))
+    rng = np.random.default_rng(seed)
 
-seed = sum(map(ord, "Logging PyMC model"))
-rng = np.random.default_rng(seed)
+    data = generate_normal_data(n=100, rng=rng, mu=2.5, sigma=3.5)
 
+    nuts_sampler, likelihood = sys.argv[1:]
+    define_model = {
+        "normal": define_normal_model,
+        "student_t": define_student_t_model,
+    }[likelihood]
 
-data = generate_normal_data(n=100, rng=rng, mu=2.5, sigma=3.5)
+    # Only MLflow related setup
+    pymc_marketing.mlflow.autolog()
 
-nuts_sampler, likelihood = sys.argv[1:]
-define_model = {
-    "normal": define_normal_model,
-    "student_t": define_student_t_model,
-}[likelihood]
+    mlflow_set_tracking_uri()
+    mlflow.set_experiment("03-pymc-autologging")
 
-# Only MLflow related setup
-pymc_marketing.mlflow.autolog()
+    with mlflow.start_run():
+        model = define_model(data)
+        idata = pm.sample(nuts_sampler=nuts_sampler, model=model)
 
-mlflow_set_tracking_uri()
-mlflow.set_experiment("03-pymc-autologging")
-
-
-mlflow.start_run()
-
-with define_model(data):
-    idata = pm.sample(nuts_sampler=nuts_sampler)
-
-    pymc_marketing.mlflow.log_inference_data(idata)
-
-mlflow.end_run()
+        pymc_marketing.mlflow.log_inference_data(idata)
