59c62043acc054e733657e97834b9411fcadf21e
Will Dean <wd60622@gmail.com>
2025-04-02 21:28:21 +0200

    add to the kick-off script

--- kick-off.sh
+++ kick-off.sh
@@ -11,6 +11,8 @@ python 03-pymc-autologging.py nutpie normal
 python 03-pymc-autologging.py pymc normal
 python 03-pymc-autologging.py numpyro normal
 python 03-pymc-autologging.py numpyro student_t
+# This will fail because of negative values in the data
+python 03-pymc-autologging.py pymc gamma
 
 export PYTHONPATH=.
 python 04-pymc-marketing-mmm
