464568a7675c927b90700c77134c7b4bcb844a6b
Will Dean <wd60622@gmail.com>
2025-08-30 11:42:00 -0400

    use argparse and add mock option

--- 03-pymc-autologging.py
+++ 03-pymc-autologging.py
@@ -5,8 +5,6 @@ for the automatic logging of various artifacts and metrics.
 
 """
 
-import sys
-
 import mlflow
 
 from utils import (
@@ -16,11 +14,38 @@ from utils import (
     generate_normal_data,
     mlflow_set_tracking_uri,
 )
+import arviz as az
 
 import pymc as pm
+from pymc.testing import mock_sample
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 import pymc_marketing.mlflow
+
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "nuts_sampler",
+    type=str,
+    choices=["pymc", "nutpie", "numpyro"],
+    help="NUTS sampler to use.",
+)
+parser.add_argument(
+    "likelihood",
+    type=str,
+    choices=["normal", "student_t", "gamma"],
+    help="Likelihood to use.",
+)
+# Option for --mock or --no-mock with default of --no-mock
+parser.add_argument(
+    "--mock",
+    action="store_true",
+    help="Use mock sampling instead of actual sampling",
+)
 
 
 if __name__ == "__main__":
@@ -29,12 +54,13 @@ if __name__ == "__main__":
 
     data = generate_normal_data(n=100, rng=rng, mu=2.5, sigma=3.5)
 
-    nuts_sampler, likelihood = sys.argv[1:]
+    args = parser.parse_args()
+
     define_model = {
         "normal": define_normal_model,
         "student_t": define_student_t_model,
         "gamma": define_gamma_model,
-    }[likelihood]
+    }[args.likelihood]
 
     # Only MLflow related setup
     pymc_marketing.mlflow.autolog()
@@ -42,9 +68,12 @@ if __name__ == "__main__":
     mlflow_set_tracking_uri()
     mlflow.set_experiment("03-pymc-autologging")
 
-    sample_kwargs = {"nuts_sampler": nuts_sampler}
+    sample_kwargs = {"nuts_sampler": args.nuts_sampler}
 
-    if nuts_sampler == "pymc":
+    if args.mock:
+        pm.sample = mock_sample
+
+    if args.nuts_sampler == "pymc":
         callback = pymc_marketing.mlflow.create_log_callback(
             stats=["energy", "model_logp", "step_size"],
             parameters=["mu", "sigma_log__"],
@@ -53,7 +82,14 @@ if __name__ == "__main__":
         sample_kwargs["callback"] = callback
 
     with mlflow.start_run():
+        mlflow.log_param("mock", args.mock)
         model = define_model(data)
         idata = pm.sample(model=model, **sample_kwargs)
 
         pymc_marketing.mlflow.log_inference_data(idata)
+
+        az.plot_forest(
+            idata,
+            var_names=["mu", "sigma"],
+        )
+        mlflow.log_figure(plt.gcf(), "forest.png")
